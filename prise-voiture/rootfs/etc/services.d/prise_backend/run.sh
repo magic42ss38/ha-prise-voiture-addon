876cc34b4c366a4e1855c752f83ba7bbfe3f537d
magic42ss38 <magic42ss38@gmail.com>
2026-03-20 18:28:15 +0100

    new version

--- prise-voiture/rootfs/etc/services.d/prise_backend/run.sh
+++ prise-voiture/rootfs/etc/services.d/prise_backend/run.sh
@@ -1,6 +1,5 @@
-#!/command/with-contenv bashio
+#!/usr/bin/with-contenv bashio
 
-# Récupération des options de l'add-on
 HA_BASE_URL=$(bashio::config 'ha_base_url')
 HA_TOKEN=$(bashio::config 'ha_token')
 
@@ -10,5 +9,4 @@ export HA_TOKEN
 bashio::log.info "Starting Prise Voiture backend (FastAPI)"
 bashio::log.info "HA_BASE_URL=${HA_BASE_URL}"
 
-# Lancer l'API FastAPI via uvicorn
-uvicorn app:app --host 0.0.0.0 --port 8000
+exec uvicorn app:app --host 0.0.0.0 --port 8000
